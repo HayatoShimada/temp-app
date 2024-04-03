f5bea5518d12d068157d1a8aea043a8b173f6b13
HayatoShimada <hayato_shimada@kanamori-system.co.jp>
2024-04-04 07:52:34 +0900

    zt

---
+++ src/app/temp/components.tsx
@@ -0,0 +1,82 @@
+// pages/temps.tsx
+import prisma from "@/app/lib/prisma";
+import { Temp } from "@prisma/client";
+
+export const Page = async () => {
+  const temps: Temp[] = await prisma.temp.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <div className="px-4 sm:px-6 lg:px-8">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">
+            温度
+          </h1>
+          <p className="mt-2 text-sm text-gray-700">
+            水槽とか
+
+          </p>
+        </div>
+
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+                  >
+                    Time
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    センサ1
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    センサ2
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    センサ3
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-gray-200">
+                {temps.map((temp) => (
+                  <tr key={temp.id}>
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                      {new Date(temp.createdAt).toLocaleString()}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {temp.temp1}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {temp.temp2}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {temp.temp3}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/temp/page.tsx
@@ -0,0 +1,11 @@
+import { Page } from "./components";
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Page />
+    </>
+  );
+};
+
+export default Home;
