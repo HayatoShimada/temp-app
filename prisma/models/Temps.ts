efb5542fdafd2c34e4250a679f205c11a7f5828d
HayatoShimada <hayato_shimada@kanamori-system.co.jp>
2024-04-04 01:48:02 +0900

    kansei

---
+++ prisma/models/Temps.ts
@@ -0,0 +1,9 @@
+export type Temp = {
+    id: string;
+    temp1: number | null;
+    temp2: number | null;
+    temp3: number | null;
+    temp4: number | null;
+    temp5: number | null;
+    createdAt: Date;
+  }
